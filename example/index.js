5de9073df3a1aaff04213a7a25d132335f112eb6
gregn <gregnb@gmail.com>
2018-07-24 22:18:18 -0400

    fix style injection bug. add support for canvas tags

--- example/index.js
+++ example/index.js
@@ -3,6 +3,14 @@ import ReactDOM from "react-dom";
 import ReactToPrint from "../src/";
 
 class ComponentToPrint extends React.Component {
+  componentDidMount() {
+
+    let ctx = this.canvasEl.getContext("2d");
+    ctx.beginPath();
+    ctx.arc(95,50,40,0,2*Math.PI);
+    ctx.stroke();
+  }
+
   render() {
     return (
       <div className={"relativeCSS"}>
@@ -27,6 +35,22 @@ class ComponentToPrint extends React.Component {
             <tr>
               <td>5</td>
               <td><img src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" width="50" /></td>
+            </tr>
+            <tr>
+              <td>svg</td>
+              <td>
+                <svg width="100" height="100">
+                  <circle cx="50" cy="50" r="40" stroke="green" strokeWidth="4" fill="yellow" />
+                </svg>
+              </td>
+            </tr>
+            <tr>
+              <td>canvas</td>
+              <td>
+                <canvas id="myCanvas" ref={el => this.canvasEl = el} width="200" height="100">
+                  Your browser does not support the HTML5 canvas tag.
+                </canvas>
+              </td>
             </tr>
           </tbody>
         </table>
