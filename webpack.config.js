2797a2a6e6ee95b6617500703f9a390f050aff9f
shmakov <shmakov@skbkontur.ru>
2019-03-22 19:34:45 +0500

    Rewrite to Typescript + fully Webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,12 @@
-const webpack = require('webpack');
+'use strict';
 
-module.exports = {
-  entry: {
-    app: './example/index.js',
-  },
-  stats: 'verbose',
-  context: __dirname,
-  output: {
-    filename: 'dist/bundle.js',
-  },
-  devtool: 'source-map',
-  devServer: {
-    disableHostCheck: true,
-    hot: true,
-    inline: true,
-    host: '0.0.0.0',
-    port: 5060,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js?$/,
-        exclude: /(node_modules)/,
-        use: 'babel-loader',
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.js'],
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NamedModulesPlugin(),
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('development'),
-      },
-    }),
-  ],
+module.exports = env => {
+    if (env.development) {
+        process.env.NODE_ENV = 'development';
+        return require("./webpack.dev.config");
+    }
+    if (env.production) {
+        process.env.NODE_ENV = 'production';
+        return require("./webpack.prod.config");
+    }
 };
