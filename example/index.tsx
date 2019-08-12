51a679cce3ecec10ddd2e9ef3cf4b0b47aa9be44
Anderson <andfariasilva@gmail.com>
2019-08-12 10:52:30 -0300

    fix errors in last commit

--- example/index.tsx
+++ example/index.tsx
@@ -28,8 +28,6 @@ class Example extends React.Component<Props, State> {
     renderTrigger = () => <button type="button">Print this out!</button>;
     onBeforeGetContent = () => new Promise((resolve, reject) => {
         this.setState({text: "text changed"}, () => resolve);
-        //if something goes wrong, regect the promise:
-        //reject();
     }));
 
     setRef = ref => this.componentRef = ref;
