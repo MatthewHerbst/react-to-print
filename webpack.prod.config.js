af4bbf612e54c32e0bdbae000590ef08da89c88c
Matthew Herbst <m@ttherb.st>
2020-04-19 19:28:31 -0700

    Rename react -> React and react-dom -> ReactDOM in webpack externals

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -27,8 +27,8 @@ module.exports = {
         ],
     },
     externals : {
-        'react': 'react',
-        'react-dom': 'react-dom'
+        'react': 'React',
+        'react-dom': 'ReactDOM'
     },
     module: {
         rules: [
