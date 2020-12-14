5e7ac119f9ed4c09af030c411c03f61ea8acd511
Matthew Herbst <m@ttherb.st>
2020-12-14 13:27:16 -0500

    Ensure we target ES5 when building in webpack, add browserslist

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -7,33 +7,49 @@ module.exports = {
     mode: 'production',
     entry:  './src/index.tsx',
     output: {
+        environment: {
+            // The environment supports arrow functions ('() => { ... }').
+            arrowFunction: false,
+            // The environment supports BigInt as literal (123n).
+            bigIntLiteral: false,
+            // The environment supports const and let for variable declarations.
+            const: false,
+            // The environment supports destructuring ('{ a, b } = obj').
+            destructuring: false,
+            // The environment supports an async import() function to import EcmaScript modules.
+            dynamicImport: false,
+            // The environment supports 'for of' iteration ('for (const x of array) { ... }').
+            forOf: false,
+            // The environment supports ECMAScript Module syntax to import ECMAScript modules (import ... from '...').
+            module: false,
+        },
         filename: 'index.js',
         path: path.resolve(__dirname, './lib'),
         libraryTarget: 'umd',
         library: 'lib',
         umdNamedDefine: true,
-        globalObject: `(typeof self !== 'undefined' ? self : this)`
+        globalObject: `(typeof self !== 'undefined' ? self : this)`,
     },
     optimization: {
         minimize: true,
     },
     externals : {
         'react': 'react',
-        'react-dom': 'react-dom'
+        'react-dom': 'react-dom',
     },
     module: {
         rules: [
             {
                 test: /\.ts(x?)$/,
                 include: path.resolve(__dirname, 'src'),
-                loader: 'ts-loader'
+                loader: 'ts-loader',
             }
         ]
     },
     resolve: {
-        extensions: ['.ts', '.tsx', '.js']
+        extensions: ['.ts', '.tsx', '.js'],
     },
     plugins: [
         new CleanWebpackPlugin(),
-    ]
+    ],
 };
